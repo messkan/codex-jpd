ac3afca03e3b4ba4abb6c59a1af2ec560b034965
skander <skanders@live.fr>
2020-04-18 19:48:20 +0100

    fix-url-env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './main/header/header.component';
-import { FooterComponent } from './main/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuSidebarComponent } from './main/menu-sidebar/menu-sidebar.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { UsersComponent } from './pages/users/users.component';
-import { AuthorizationService } from '../app/utils/services/authorization.service';
-import { ErrorComponent } from './error/error.component';
-import { JwtModule } from '@auth0/angular-jwt';
-import { UsersUpdateComponent } from './pages/users/users-update/users-update.component';
-import { UsersAddComponent } from './pages/users/users-add/users-add.component';
-import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./main/main.component";
+import { LoginComponent } from "./login/login.component";
+import { HeaderComponent } from "./main/header/header.component";
+import { FooterComponent } from "./main/footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MenuSidebarComponent } from "./main/menu-sidebar/menu-sidebar.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
+import { UsersComponent } from "./pages/users/users.component";
+import { AuthorizationService } from "../app/utils/services/authorization.service";
+import { ErrorComponent } from "./error/error.component";
+import { JwtModule } from "@auth0/angular-jwt";
+import { UsersUpdateComponent } from "./pages/users/users-update/users-update.component";
+import { UsersAddComponent } from "./pages/users/users-add/users-add.component";
+import { UsersListComponent } from "./pages/users/users-list/users-list.component";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
 
-const apiUrl = environment.apiUrl;  
-
+const apiUrl = environment.apiUrl;
+const domain = environment.domain;
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +36,7 @@ const apiUrl = environment.apiUrl;
     ErrorComponent,
     UsersUpdateComponent,
     UsersAddComponent,
-    UsersListComponent
+    UsersListComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,19 +46,20 @@ const apiUrl = environment.apiUrl;
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       timeOut: 1100,
-      positionClass: 'toast-bottom-right',
-      preventDuplicates: true
-    }) , 
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true,
+    }),
     JwtModule.forRoot({
       config: {
-        tokenGetter: function  tokenGetter() {
-             return     localStorage.getItem('token');},
-        whitelistedDomains: ['localhost:3000'],
-        blacklistedRoutes: ['http://localhost:3000/auth/login']
-      }
-    })
+        tokenGetter: function tokenGetter() {
+          return localStorage.getItem("token");
+        },
+        whitelistedDomains: [`${domain}`],
+        blacklistedRoutes: [`${apiUrl}/auth/login`],
+      },
+    }),
   ],
   providers: [AuthorizationService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:3000',
-  production: false
+  apiUrl: "http://101f5507.ngrok.io",
+  domain: "101f5507.ngrok.io",
+  production: false,
 };
 
 /*
